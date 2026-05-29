a9efee520f17a4733d9d42f013215b3180b6798c
Will Bickel <furiouzdesigns@gmail.com>
2026-05-29 17:10:24 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -28,7 +28,7 @@ export default function Layout() {
               <img
                 src="https://media.base44.com/images/public/69f28176704facfd454194e1/3dbb86754_Untitleddesign2.svg"
                 alt="Stack Sixth"
-                className="h-10 object-contain"
+                className="h-14 object-contain"
               />
             </Link>
             <nav className="flex items-center gap-1">
