b244e7df5f18d0c6358fcd024be61599a595a518
Will Bickel <furiouzdesigns@gmail.com>
2026-05-29 17:09:56 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -26,7 +26,7 @@ export default function Layout() {
           <div className="flex items-center justify-between h-16">
             <Link to="/" className="flex items-center">
               <img
-                src="https://media.base44.com/images/public/69f28176704facfd454194e1/c66e4be1b_Untitleddesign1.svg"
+                src="https://media.base44.com/images/public/69f28176704facfd454194e1/3dbb86754_Untitleddesign2.svg"
                 alt="Stack Sixth"
                 className="h-10 object-contain"
               />
